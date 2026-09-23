bd04513810f91d1d6ffa531ce8b7e726ba166244
Carlos Celi <62081230+Normando1945@users.noreply.github.com>
2026-09-23 08:17:07 -0500

    update 03

--- repo__CORE_concreteII_desing/core.py
+++ repo__CORE_concreteII_desing/core.py
@@ -32,12 +32,13 @@ class SimpleMatrixStack:
 #########################################################################################################################################
 #########################################################################################################################################
 class SimpleClass():
-    def __init__(self, x = any,y = any, title = 'test', xlabel = 'x', ylabel = 'y'):
+    def __init__(self, x = any,y = any, title = 'test', xlabel = 'x', ylabel = 'y', color = (0,0,0)):
         self.x = x                                                      # Store x in the instance
         self.y = y                                                      # Store y in the instance
         self.title = title                                              # Store the title
         self.xlabel = xlabel                                            # Store the x label
         self.ylabel = ylabel                                            # Store the y label
+        self.color = color
 
     def SimplePlot(self):
         x = self.x                                                      # Read x from the instance
@@ -45,9 +46,11 @@ class SimpleClass():
         title = self.title                                              # Read the title
         xlabel = self.xlabel                                            # Read the x label
         ylabel = self.ylabel                                            # Read the y label
+        color = self.color
+
 
         fig, ax = plt.subplots(1,1, figsize = (15,6))                   # Create a figure with one subplot
-        ax.plot(x,y, ls = '-', lw = 2, color = (0,0,1), label = title)  # Draw the curve
+        ax.plot(x,y, ls = '-', lw = 2, color = color, label = title)  # Draw the curve
         ax.set_title(title, color = (0,0,0), fontsize = 14)             # Add a title
         ax.set_xlabel(xlabel)                                           # Label the x axis
         ax.set_ylabel(ylabel)                                           # Label the y axis
